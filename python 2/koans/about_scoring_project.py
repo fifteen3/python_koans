e5d54f62b80b13457d21c8245c5b41a3b2a6a931
Carlo Costantini <carlo@carlocostantini.ca>
2016-09-09 11:19:16 -0400

    passing about scoring project

--- python 2/koans/about_scoring_project.py
+++ python 2/koans/about_scoring_project.py
@@ -35,41 +35,55 @@ from runner.koan import *
 
 def score(dice):
     # You need to write this method
-	if  dice == []:
-		return 0
+    points = {}.fromkeys(range(0, 10),0)
+    sum = 0
+
+    for die in dice:
+        points[die] += 1
+
+    sum += (points[1] // 3) * 1000
+
+    sum += (points[1] % 3) * 100
+
+    sum += (points[5] % 3) * 50
+
+    for r in range(2,10):
+        sum += (points[r] // 3) * 100 * r
+
+    return sum
 
 
 class AboutScoringProject(Koan):
     def test_score_of_an_empty_list_is_zero(self):
         self.assertEqual(0, score([]))
-    
+
     def test_score_of_a_single_roll_of_5_is_50(self):
         self.assertEqual(50, score([5]))
-    
+
     def test_score_of_a_single_roll_of_1_is_100(self):
         self.assertEqual(100, score([1]))
-    
+
     def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores(self):
         self.assertEqual(300, score([1, 5, 5, 1]))
-    
+
     def test_score_of_single_2s_3s_4s_and_6s_are_zero(self):
         self.assertEqual(0, score([2, 3, 4, 6]))
-    
+
     def test_score_of_a_triple_1_is_1000(self):
         self.assertEqual(1000, score([1, 1, 1]))
-    
+
     def test_score_of_other_triples_is_100x(self):
         self.assertEqual(200, score([2, 2, 2]))
         self.assertEqual(300, score([3, 3, 3]))
         self.assertEqual(400, score([4, 4, 4]))
         self.assertEqual(500, score([5, 5, 5]))
         self.assertEqual(600, score([6, 6, 6]))
-    
+
     def test_score_of_mixed_is_sum(self):
         self.assertEqual(250, score([2, 5, 2, 2, 3]))
         self.assertEqual(550, score([5, 5, 5, 5]))
         self.assertEqual(1150, score([1, 1, 1, 5, 1]))
-        
+
     def test_ones_not_left_out(self):
         self.assertEqual(300, score([1, 2, 2, 2]))
         self.assertEqual(350, score([1, 5, 2, 2, 2]))
